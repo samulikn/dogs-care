de169bf87fac94c450fdbc3bf1f936831348573f
Nataliia Shobotova <samulikn@gmail.com>
2026-05-12 13:42:48 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex flex-col justify-center p-3">
+      <h1 className="mx-auto">Tell us more about your pup</h1>
+    </main>
+  );
+}
+
+export default App;
